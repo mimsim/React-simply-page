190fd1126bd7320f122b3af17650f3f73c78695e
mimsim <mimsim2000@gmail.com>
2022-01-31 19:42:25 +0200

    add input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Input from "../../shared/components/FormElements/Input";
+
+import './NewPlace.css'
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" validators={[]} onChange={}/>
+      <Input type="text" label="Title" validator={[]} errorText="Invalid Title"/>
     </form>
   );
 };
